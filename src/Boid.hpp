6947ca83dd084053e49cfb199b2141746bc2b58b
odewitness <audepora82@orange.fr>
2023-03-15 15:03:06 +0100

    🧹 Nettoyage

--- src/Boid.hpp
+++ src/Boid.hpp
@@ -16,7 +16,6 @@ private:
 
 public:
     Boid();
-    // Boid(glm::vec2 position, glm::vec2 vitesse, float radius);
     Boid(glm::vec2 position, glm::vec2 vitesse, float radius, float m_top_limit, float m_bottom_limit, float m_left_limit, float m_right_limit);
     // ~Boid();
 
